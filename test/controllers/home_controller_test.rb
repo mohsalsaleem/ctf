329cdfb97ca1043758101d4e2b30ece42ce818f9
Prashant <prashant.barca@gmail.com>
2014-07-18 22:13:16 +0530

    first commit

---
+++ test/controllers/home_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class HomeControllerTest < ActionController::TestCase
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
+
+  test "should get about" do
+    get :about
+    assert_response :success
+  end
+
+  test "should get team" do
+    get :team
+    assert_response :success
+  end
+
+end
