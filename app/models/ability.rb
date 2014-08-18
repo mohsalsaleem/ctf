2c83ec2b374016ca78e0b2e6911c8fb1da304899
Senthil Ramasamy <www.senthilr.com@gmail.com>
2014-08-18 07:09:31 +0530

    added n configured rolify with admin role

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,7 +2,11 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    can :read, :all
+    if user.has_role? :admin
+        can :manage, :all
+    else
+        can :read, :all
+    end
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,5 @@
 class Event < ActiveRecord::Base
 	has_attached_file :featured_image, :styles => { :large => "800x400" ,:medium => "300x300>", :thumb => "100x100>" }, :default_url => "/assets/default.jpeg"
   	validates_attachment_content_type :featured_image, :content_type => /\Aimage\/.*\Z/
+  	resourcify
 end
